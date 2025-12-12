22291b0825cc6f9bcba49d7e726a99b9be8cf2d6
aw1app <aw1app@outlook.com>
2025-12-12 11:42:53 +0530

    angular updates

--- ANGULAR/my-first-project/src/app/app.ts
+++ ANGULAR/my-first-project/src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Product } from './components/product/product';
+import { User } from './components/user/user';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Product, User],
   templateUrl: './app.html',
+  //template:'Hi {{x}} ',
   styleUrl: './app.css'
 })
 export class App {
---
+++ ANGULAR/my-first-project/src/app/components/product/product.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'product',
+  imports: [],
+  templateUrl: './product.html',
+  styleUrl: './product.css',
+})
+export class Product {
+
+  // id! : number;
+  // name!: string;
+  // price!:number;
+
+  // Inputs from the parent component
+  @Input("id") id!: number;
+  @Input("name") name!: string;
+  @Input("price") price!: number;
+
+  description:string = "Default description";
+
+
+}
