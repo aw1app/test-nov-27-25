7be6af6b4ff5bc1f9deb7ac2774bdb854dc14fa6
aw1app <aw1app@outlook.com>
2026-01-07 14:23:38 +0530

    core java updates

--- PHASE2/JAVA/FirstProject/src/com/sl/MethodsDemo.java
+++ PHASE2/JAVA/FirstProject/src/com/sl/MethodsDemo.java
@@ -5,20 +5,19 @@ public class MethodsDemo {
 	public static void main(String[] args) {
 		
 		int result1 = add(4,5);
-		System.out.println("result1="+result1);
-		
+		System.out.println("result1="+result1);		
 
 	}
 	
 	// method example
 	static int add(int x,int y){
-	
-		int c = x+y;
-		return c ;
 		
+		int c = x+y;
+		return c ;		
 	}
 	
-	//TASK-1: write a method that will accept salary and will compute & return the 15% tax amount to be paid.
+	//TASK-1: write a method that will accept salary and will compute 
+	// & return the 15% tax amount to be paid.
 	
 
 }
