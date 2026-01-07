3133ede99b319cdb7e0825da21c795b178bef46d
aw1app <aw1app@outlook.com>
2026-01-07 12:54:21 +0530

    core java updates

--- PHASE2/JAVA/FirstProject/src/com/sl/ArraysDemo.java
+++ PHASE2/JAVA/FirstProject/src/com/sl/ArraysDemo.java
@@ -12,10 +12,23 @@ public class ArraysDemo {
 		System.out.println(myArray[0]);
 
 		// looping through all elements of this array
-		System.out.print("\n\n looping through all elements of this array");
+		System.out.print("\n\n looping through all elements of this array\n");
 		for (int i = 0; i < myArray.length; i++) {
 			System.out.println("myArray[" + i + "] : " + myArray[i]);
 		}
+		
+		
+		//Multi-Dimensional Array
+		System.out.print("\n\n Multi-Dimensional Array\n");
+		int[][] numbers = {
+				 {1, 2, 3, 4, 5},
+				 {11, 12, 13, 14, 15}
+				 };
+		
+		// print the 0,0 (row=0, col=0) element
+		System.out.println("0,0 (row=0, col=0) element is : " + numbers[0][0]); // 1
+		System.out.println("1,3 (row=1, col=3) element is : " + numbers[1][3]); // 14 
+		
 	}
 
 }
