44fd13cb9f27434728dee50e5290ca9516b7bc61
aw1app <aw1app@outlook.com>
2026-01-07 14:20:58 +0530

    core java updates

---
+++ PHASE2/JAVA/FirstProject/src/com/sl/MethodsDemo.java
@@ -0,0 +1,21 @@
+package com.sl;
+
+public class MethodsDemo {
+
+	public static void main(String[] args) {
+		
+		int result1 = add(4,5);
+		System.out.println("result1="+result1);
+		
+
+	}
+	
+	// method example
+	static int add(int x,int y){
+	
+		int c = x+y;
+		return c ;
+		
+	}
+
+}
