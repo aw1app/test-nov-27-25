0469d3386b2f9442a0b04eb6015bf851d61acbbd
aw1app <aw1app@outlook.com>
2026-01-09 15:28:00 +0530

    core java multi-threading synchronized wait notify keyword demo

--- PHASE2/JAVA/MultiThreadingDemo2/src/SharedBuffer.java
+++ PHASE2/JAVA/MultiThreadingDemo2/src/SharedBuffer.java
@@ -6,17 +6,23 @@ public class SharedBuffer {
 	private final Queue<Integer> queue = new LinkedList<>();
 	private final int CAPACITY = 5;
 
-	synchronized void produce(int value) {
+	synchronized void produce(int value) throws InterruptedException {
 		if (queue.size() < 5) {
 			queue.add(value);
 			System.out.println("Produced: " + value);
+			notifyAll();
+		}else {
+			wait();
 		}
 	}
 
-	synchronized void consume() {
+	synchronized void consume() throws InterruptedException {
 		if (!queue.isEmpty()) {
 			int value = queue.remove();
 			System.out.println("Consumed: " + value);
+			notifyAll();
+		}else {
+			wait();
 		}
 	}
 
