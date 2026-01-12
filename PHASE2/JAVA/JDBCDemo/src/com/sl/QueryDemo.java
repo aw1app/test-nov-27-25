d9d12b692f66cb2cb246fe41950f4cc09b6c6fdc
aw1app <aw1app@outlook.com>
2026-01-12 12:06:57 +0530

    JDBC demo

---
+++ PHASE2/JAVA/JDBCDemo/src/com/sl/QueryDemo.java
@@ -0,0 +1,54 @@
+package com.sl;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class QueryDemo {
+
+	public static void main(String[] args) {
+		
+		//1. Create the connection object
+		Connection connection = null;
+		
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/cis_estore", "root", "rootroot");
+			System.out.println("Connection to MySQL DB Established");
+		} catch (SQLException e) {
+			System.out.println("Connection to MySQL DB Failed with exception " + e);
+		}
+		
+		// 2 Create the statement object
+		Statement stmt = null;
+		try {
+			stmt = connection.createStatement();
+			System.out.println("Created the Statement object successfully!");
+		} catch (SQLException e) {
+			System.out.println("Creating the Statement object failed " + e);
+		}
+		
+		// 3. Execute the statement
+		String sqlCommand = "select * from products";
+		try {
+			ResultSet resultSet = stmt.executeQuery(sqlCommand);
+			System.out.println("Executed the Statement object successfully!");
+			
+			// Navigate the results (rows)
+			System.out.println("List of products in the products table");
+			while(resultSet.next()) {
+				int id = resultSet.getInt("id");
+				String name = resultSet.getString("name");
+				float price = resultSet.getFloat("price");
+				
+				System.out.printf("%3s %20s %10s \n", id, name, price);				
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("Executing the Statement object failed!!" + e);
+		}
+
+	}
+
+}
