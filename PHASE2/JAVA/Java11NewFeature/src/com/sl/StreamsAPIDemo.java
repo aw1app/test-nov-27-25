a56c7de96ac57a992c60fc6b04b4f8e8f858bf03
aw1app <aw1app@outlook.com>
2026-01-12 11:02:15 +0530

    core java - streams api demo

--- PHASE2/JAVA/Java11NewFeature/src/com/sl/StreamsAPIDemo.java
+++ PHASE2/JAVA/Java11NewFeature/src/com/sl/StreamsAPIDemo.java
@@ -27,10 +27,28 @@ public class StreamsAPIDemo {
 		System.out.println("\n first three fruits using .limit of Streams API");
 		fruitsFirstThree.stream().forEach(  frt -> System.out.println(frt)  );
 		
-		// filter those fruits that are having names lenght > 5
+		// filter those fruits that are having names lenght < 5
 		System.out.println("\n names of fruits LT 5 chars using .filter of Streams API");
 		List<String> fruitsNamesLengthLT5 = fruits.stream().filter(  frt ->  frt.length() <= 5  ).toList();
 		fruitsNamesLengthLT5.stream().forEach(  frt -> System.out.println(frt)  );
+		
+		// mapping or transformations
+		System.out.println("\n\n mapping or transformations demo using .map of Streams API");
+		List<String> fruitsinCaps= fruits
+		.stream()
+		.map (frt -> frt.toUpperCase())
+		.toList();
+		
+		fruitsinCaps.stream().forEach(  frt -> System.out.println(frt)  );
+		
+		// Chaining of stream ops
+		System.out.println("\n\n Chaining of stream ops");
+		fruits
+		.stream()
+		.map (frt -> frt.toUpperCase())
+		.filter (frt -> frt.startsWith("A"))
+		.forEach(  frt -> System.out.println(frt)  );
+		
 		
 	}
 
