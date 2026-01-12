fcd3306f849f89dd5da80a5fd0b8ba62f46ebc72
aw1app <aw1app@outlook.com>
2026-01-12 12:40:19 +0530

    JDBC create table statement demo

---
+++ PHASE2/JAVA/JDBCDemo/src/com/sl/DeleteDemo.java
@@ -0,0 +1,48 @@
+package com.sl;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DeleteDemo {
+
+	public static void main(String[] args) {
+		
+		//1. Create the connection object
+		Connection connection = null;
+		
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/cis_estore", "root", "rootroot");
+			System.out.println("Connection to MySQL DB Established");
+		} catch (SQLException e) {
+			System.out.println("Connection to MySQL DB Failed with exception " + e);
+		}
+		
+		// 2 Create the statement object
+		Statement stmt = null;
+		try {
+			stmt = connection.createStatement();
+			System.out.println("Created the Statement object successfully!");
+		} catch (SQLException e) {
+			System.out.println("Creating the Statement object failed " + e);
+		}
+		
+		// 3. Execute the statement
+		String sqlCommand = "DELETE from products WHERE name like '%1022%'";
+		try {
+			int count = stmt.executeUpdate(sqlCommand);
+			System.out.println("Executed the Statement object successfully!");
+			
+			// Navigate the results (rows)
+			System.out.printf("Deleted %d row(s) successfully where the product name had '1022'!",count);
+			
+			
+		} catch (SQLException e) {
+			System.out.println("Executing the Statement object failed!!" + e);
+		}
+
+	}
+
+}
