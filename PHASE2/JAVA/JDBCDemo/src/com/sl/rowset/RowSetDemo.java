3b0762f563e607172718cc58e10e893b94f947d9
aw1app <aw1app@outlook.com>
2026-01-13 10:24:08 +0530

    JDBC rowset demo updates

--- PHASE2/JAVA/JDBCDemo/src/com/sl/rowset/RowSetDemo.java
+++ PHASE2/JAVA/JDBCDemo/src/com/sl/rowset/RowSetDemo.java
@@ -1,5 +1,8 @@
 package com.sl.rowset;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.sql.rowset.CachedRowSet;
@@ -13,28 +16,53 @@ public class RowSetDemo {
 	private static final String PASSWORD = "rootroot";
 
 	public static void main(String[] args) throws SQLException {
-		demonstrateCachedRowSet();
+		Connection con = DriverManager.getConnection(URL, USER, PASSWORD);
+		con.setAutoCommit(false);
+
+		demonstrateCachedRowSet(con);
+
+		con.close();
 	}
 
-	private static void demonstrateCachedRowSet() throws SQLException {
+	private static void demonstrateCachedRowSet(Connection con) throws SQLException {
 
 		RowSetFactory factory = RowSetProvider.newFactory();
 
 		try (CachedRowSet rowSet = factory.createCachedRowSet()) {
-			loadInitialData(rowSet);
+			loadInitialData(rowSet, con);
 
 			System.out.println("Initial product data:");
 			printRowSet(rowSet);
+
+			System.out.println("\nMaking offline changes...");
+			rowSet.beforeFirst();
+			rowSet.absolute(5); // go to fifth 5 row
+			rowSet.updateFloat("price", 20000.0f);
+			rowSet.updateRow();
+
+			System.out.println("\nOffline changes successful...");
+
+			try {
+				rowSet.acceptChanges(con);
+				con.commit();
+			} catch (Exception e) {
+				con.rollback();
+				System.err.println("Rollback occurred");
+				throw e;
+			}
+			System.out.println("Changes written to database");
 		}
 
 	}
 
-	private static void loadInitialData(CachedRowSet rowSet) throws SQLException {
-		rowSet.setUrl(URL);
-		rowSet.setUsername(USER);
-		rowSet.setPassword(PASSWORD);
-		rowSet.setCommand("SELECT * FROM products ORDER BY id");
-		rowSet.execute();
+	private static void loadInitialData(CachedRowSet rowSet, Connection con) throws SQLException {
+
+		rowSet.setTableName("products");
+		rowSet.setKeyColumns(new int[] { 1 }); // id the primary
+		rowSet.setConcurrency(ResultSet.CONCUR_UPDATABLE);
+
+		rowSet.setCommand("SELECT * FROM products");
+		rowSet.execute(con);
 	}
 
 	private static void printRowSet(CachedRowSet rowSet) throws SQLException {
@@ -44,7 +72,7 @@ public class RowSetDemo {
 		// Print each row with proper formatting
 		while (rowSet.next()) {
 			try {
-				System.out.printf("%-4d  %-20s  %,12.2f \n", rowSet.getInt("id"), rowSet.getString("name"),
+				System.out.printf("%-4d  %-20s  %12.2f \n", rowSet.getInt("id"), rowSet.getString("name"),
 						rowSet.getFloat("price"));
 			} catch (SQLException e) {
 				System.err.printf("Error reading row: %s%n", e.getMessage());
