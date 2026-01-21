3634ac46c7f79a98b28aea1c851d120727adcea7
aw1app <aw1app@outlook.com>
2026-01-21 14:24:54 +0530

    java servlets demo

---
+++ PHASE2/JAVA/ServletDemo/src/main/java/com/sl/WelcomeServlet.java
@@ -0,0 +1,32 @@
+package com.sl;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/WelcomeServlet")
+public class WelcomeServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		PrintWriter out = response.getWriter();
+
+		out.println("Hello");
+		out.println("My");
+		out.println("Name");
+		out.println("Is");
+		out.println("Ryan");
+		out.println("Thanks for reaching us !");
+		out.println("Have a nice day :) ");
+
+		out.close();
+
+	}
+
+}
