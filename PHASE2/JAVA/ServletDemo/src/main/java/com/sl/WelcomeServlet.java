801a08baa99cf57ba1cba0107e0e82420d21d0e3
aw1app <aw1app@outlook.com>
2026-01-21 15:09:40 +0530

    java servlets mapping demo

--- PHASE2/JAVA/ServletDemo/src/main/java/com/sl/WelcomeServlet.java
+++ PHASE2/JAVA/ServletDemo/src/main/java/com/sl/WelcomeServlet.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/WelcomeServlet")
+//@WebServlet("/welcome")
 public class WelcomeServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
