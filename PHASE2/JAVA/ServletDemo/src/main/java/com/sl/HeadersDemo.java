1bc1bd17a7d768aa1281af6e26a0d6bf2f33067a
aw1app <aw1app@outlook.com>
2026-01-22 09:49:56 +0530

    java servlets headers demo demo

---
+++ PHASE2/JAVA/ServletDemo/src/main/java/com/sl/HeadersDemo.java
@@ -0,0 +1,29 @@
+package com.sl;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/headers-demo")
+public class HeadersDemo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+
+		PrintWriter out = response.getWriter();
+
+		String userAgent = request.getHeader("User-Agent");
+		out.println("Your are aacessing this servlet from browser : " + userAgent);
+		
+
+		out.close();
+	}
+
+}
