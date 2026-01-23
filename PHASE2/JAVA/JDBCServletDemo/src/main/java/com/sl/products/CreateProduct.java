9d653c3d26066609e49d79120fbf52ac4d312783
aw1app <aw1app@outlook.com>
2026-01-23 14:38:22 +0530

    java servlets - db connection insert product demo.

--- PHASE2/JAVA/JDBCServletDemo/src/main/java/com/sl/products/CreateProduct.java
+++ PHASE2/JAVA/JDBCServletDemo/src/main/java/com/sl/products/CreateProduct.java
@@ -1,12 +1,11 @@
-package com.sl;
+package com.sl.products;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletContext;
@@ -16,15 +15,13 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/query-servlet")
-public class QueryServlet extends HttpServlet {
+@WebServlet("/create-product")
+public class CreateProduct extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	ServletContext context;
-	
-	Connection connection = null;
 
-	int counter;
+	Connection connection = null;
 
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config); // don't forget this call
@@ -38,44 +35,44 @@ public class QueryServlet extends HttpServlet {
 
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			connection = DriverManager.getConnection(dbURL,dbUser, dbPass);
+			connection = DriverManager.getConnection(dbURL, dbUser, dbPass);
 			System.out.println("Connection to MySQL DB Established");
 		} catch (SQLException | ClassNotFoundException e) {
 			System.out.println("Connection to MySQL DB Failed with exception " + e);
-		} 
+		}
 	}
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		
-		out.append("<br> INSIDE QueryServlet");
-		
-		Statement stmt = null;
+
+		out.append("<br> INSIDE Create Product");
+		String name = request.getParameter("name");
+		float price = Float.parseFloat(request.getParameter("price"));
+
+		PreparedStatement preparedStmt = null;
 		try {
-			stmt = connection.createStatement();
-			String sqlCommand = "select * from products";
-			ResultSet resultSet = stmt.executeQuery(sqlCommand);
+			String sqlCommand = "INSERT INTO products(name,price) values(?,?)";
+			preparedStmt = connection.prepareStatement(sqlCommand);
 			
-			out.printf("<br><table border='1'>");
-			while(resultSet.next()) {
-				int id = resultSet.getInt("id");
-				String name = resultSet.getString("name");
-				float price = resultSet.getFloat("price");
-				
-				out.printf("<tr> <td>%3s <td>%20s <td>%10s </tr>", id, name, price);				
-			}
-			out.printf("</table><br>");
+			preparedStmt.clearParameters();
+			preparedStmt.setString(1, name);
+			preparedStmt.setFloat(2, price);
+			
+			int rowcount = preparedStmt.executeUpdate();
+			System.out.println("no of  rows updated "+rowcount);
+			out.printf("Product Inserted successfully. <br>");
 			
 		} catch (SQLException e) {
-			System.out.println("Creating the Statement object failed " + e);
-		}	
+			System.out.println("INSERT product  failed " + e);
+		};
 		
-		
+		out.printf("<a href=\"index.html\" >Home</a><br>");
+
 		out.close();
 	}
-	
+
 	public void destroy() {
 		try {
 			connection.close();
