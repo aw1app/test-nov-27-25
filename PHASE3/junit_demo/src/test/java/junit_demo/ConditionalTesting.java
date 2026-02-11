b2a5c5ec2f684eb0bcb465e9af3c45fa62a4cfb1
aw1app <aw1app@outlook.com>
2026-02-11 15:02:08 +0530

    Junit more conditional testing

--- PHASE3/junit_demo/src/test/java/junit_demo/ConditionalTesting.java
+++ PHASE3/junit_demo/src/test/java/junit_demo/ConditionalTesting.java
@@ -1,7 +1,12 @@
 package junit_demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.junit.jupiter.api.condition.EnabledOnJre;
 import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.JRE;
 import org.junit.jupiter.api.condition.OS;
 
 class ConditionalTesting {
@@ -13,5 +18,69 @@ class ConditionalTesting {
 	}
 	
 	//Task-3: Write a test case that will run only on Mac OS systems
+	
+	@Test
+    @EnabledOnOs({OS.MAC})
+    void testOnMac() {
+        System.out.println("INSIDE testOnMac");
+    }
+	
+	@Test
+	@EnabledOnOs({ OS.LINUX })
+	public void testAddOnLinux() {
+		
+		System.out.println("INSIDE testAddOnLinux");
+		
+		Calculator cal = new Calculator();
+
+		int x = 2;
+		int y = 5;
+
+		assertEquals(7, cal.add(x, y));
+	}
+	
+	
+	// based JRE
+	@Test
+	@EnabledOnJre({JRE.JAVA_21})
+	public void testAddJRE21() {
+		
+		System.out.println("INSIDE testAddJRE21");
+		
+		Calculator cal = new Calculator();
+
+		int x = 2;
+		int y = 5;
+
+		assertEquals(7, cal.add(x, y));
+	}
+	
+	@Test
+	@EnabledOnJre({JRE.JAVA_26})
+	public void testAddJRE26() {
+		
+		System.out.println("INSIDE testAddJRE26");
+		
+		Calculator cal = new Calculator();
+
+		int x = 2;
+		int y = 5;
+
+		assertEquals(7, cal.add(x, y));
+	}
+	
+	@Test
+	@EnabledIfEnvironmentVariable(named = "x1", matches = "\\d{3}")
+	public void testAddEnvVariable() {
+		
+		System.out.println("INSIDE testAddEnvVariable for variable x1 which is a 3 digit number");
+		
+		Calculator cal = new Calculator();
+
+		int x = 2;
+		int y = 5;
+
+		assertEquals(7, cal.add(x, y));
+	}
 
 }
