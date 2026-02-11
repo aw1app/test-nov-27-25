deb5dbba722ba8d4ec44a04798fbb89dabb5dbc1
aw1app <aw1app@outlook.com>
2026-02-11 15:48:05 +0530

    Junit repeated test cases

--- PHASE3/junit_demo/src/test/java/junit_demo/ParametrizedTests.java
+++ PHASE3/junit_demo/src/test/java/junit_demo/ParametrizedTests.java
@@ -2,7 +2,7 @@ package junit_demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -17,6 +17,7 @@ class ParametrizedTests {
 			}
 			)
 	void testAdd(int a, int b, long expectedResult) {
+		System.out.printf("INSIDE testAdd with a=%s b=%s expectedResult=%s \n", a,b,expectedResult);
 		Calculator calc = new Calculator();
 		
 
@@ -24,6 +25,18 @@ class ParametrizedTests {
 
 		// check the output and assert it is equal to expectedResult
 		assertEquals(expectedResult, actualResult);
+	}
+	
+	
+	@RepeatedTest(6)
+	void testRepeatedlyAdd() {
+		System.out.println("INSIDE testRepeatedlyAdd");
+		Calculator calc = new Calculator();		
+
+		long actualResult = calc.add(1, 1);
+
+		// check the output and assert it is equal to expectedResult
+		assertEquals(2, actualResult, "1 + 1 equals 2");
 	}
 
 }
