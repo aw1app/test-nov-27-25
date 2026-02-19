f71c3be8117f65ff5f739e95d670b189a8eb9579
aw1app <aw1app@outlook.com>
2026-02-19 15:44:17 +0530

    Spring Boot JPA demo Part 2

---
+++ PHASE3/spring-data-jpa-demo/src/main/java/com/sl/entity/Product.java
@@ -0,0 +1,34 @@
+package com.sl.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="products")
+@Data // will add getters and setter in to the class
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	int id;
+	
+	@Column(name="name")
+	String name;
+	
+	@Column(name="price")
+	float price;
+	
+	@Column(name="decription")
+	String decription;	
+
+}
