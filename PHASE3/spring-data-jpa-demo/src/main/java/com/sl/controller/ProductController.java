ad5add4fb8c4e1d68e2dfed5561c5309aca57000
aw1app <aw1app@outlook.com>
2026-02-20 09:54:44 +0530

    Spring Boot JPA - CRUD demo updated

--- PHASE3/spring-data-jpa-demo/src/main/java/com/sl/controller/ProductController.java
+++ PHASE3/spring-data-jpa-demo/src/main/java/com/sl/controller/ProductController.java
@@ -1,11 +1,13 @@
 package com.sl.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sl.entity.Product;
@@ -14,17 +16,36 @@ import com.sl.repository.ProductRepository;
 @Controller
 @RequestMapping("products")
 public class ProductController {
-	
+
 	@Autowired
 	ProductRepository productRepository;
-	
+
 	@GetMapping("/list")
 	public String getAllProducts(ModelMap model) {
-		List<Product> products =productRepository.findAll();
-		
+		List<Product> products = productRepository.findAll();
+
 		model.addAttribute("products", products);
-		
+
 		return "list-products"; // WEB-INF/views/list-products.jsp
-	} 
+	}
+
+	// detail a single product
+	@GetMapping("/details/{id}")
+	public String getAllProducts(ModelMap model, @PathVariable int id) {
+		Optional<Product> optionalProduct = productRepository.findById(id);
+
+		if (optionalProduct.isPresent()) {
+			Product product = optionalProduct.get();
+			model.addAttribute("product", product);
+		}
+
+		return "product"; // WEB-INF/views/product.jsp
+	}
+
+	// insert a new product
+
+	// delete a product
+
+	// edit and update a product
 
 }
---
+++ PHASE3/spring-data-jpa-demo/src/main/webapp/WEB-INF/views/product.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="jakarta.tags.core" prefix="c"%>
+<!DOCTYPE html>
+
+<br>
+DETAILS OF PRODUCT
+<br>
+<c:if  test="${product!=null}">
+	<ul>
+		<li>ID: ${product.id }
+		<li>Name: ${product.name }
+		<li>Price: ${product.price}
+		<li>Description: ${product.description}
+	</ul>
+</c:if>	
