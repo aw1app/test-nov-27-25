e381fe1f568dd5212509d100752d65dc603edf87
aw1app <aw1app@outlook.com>
2026-02-26 12:52:42 +0530

    spring rest client - task 25 delete ops

--- PHASE3/spring-boot-rest-client-demo/src/main/java/com/sl/controller/TodoController.java
+++ PHASE3/spring-boot-rest-client-demo/src/main/java/com/sl/controller/TodoController.java
@@ -7,31 +7,44 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.sl.entity.Todo;
-
 
 @Controller
 @RequestMapping("todos")
 public class TodoController {
-	
+
 	@Autowired
 	RestClient restClientTodos;
-	
-	@GetMapping("/list")	
+
+	@GetMapping("/list")
 	public String listTodos(Model model) {
-		
-		List<Todo> todos = restClientTodos
-		.get()
-		.uri("/todos")
-		.retrieve()
-		.body(new ParameterizedTypeReference<List<Todo>>() {});
-		
+
+		List<Todo> todos = restClientTodos.get().uri("/todos").retrieve()
+				.body(new ParameterizedTypeReference<List<Todo>>() {} );
+
 		model.addAttribute("todos", todos);
-		
+
 		return "list"; // WEB-INF/views/list.jsp
+	}
+
+	// Task -24 : Implement fetching and displaying a single todo
+
+	// Task -25 : Implement deleting a single todo
+	// Delete method
+	@GetMapping("/delete-todo/{id}")
+	public String deleteTodo(@PathVariable int id, RedirectAttributes redirectAttributes) {
+		restClientTodos.delete().uri("/todos/{id}", id).retrieve().toBodilessEntity();
+
+		// Add a flash message for the user
+		redirectAttributes.addFlashAttribute("message", "Todo #" + id + " deleted successfully (simulated)!");
+
+		// return to the list page
+		return "redirect:/todos/list";
 	}
 
 }
--- PHASE3/spring-boot-rest-client-demo/src/main/webapp/WEB-INF/views/list.jsp
+++ PHASE3/spring-boot-rest-client-demo/src/main/webapp/WEB-INF/views/list.jsp
@@ -3,24 +3,34 @@
 <%@ taglib uri="jakarta.tags.core" prefix="c"%>
 <!DOCTYPE html>
 
+<c:if test="${not empty message}">
+	<div style="color: blue; margin-bottom: 10px;">${message}</div>
+</c:if>
+
 <br>
 LIST OF TODOS
 <br>
 <table border=1>
 	<tr style="background-color: lightgrey">
 		<th>ID
-		<th>USER ID 
+		<th>USER ID
 		<th>TITLE
 		<th>STATUS
+		<th>DELETE
 	</tr>
 
 	<c:forEach var="todo" items="${todos}">
 		<tr>
-			<td>${todo.id }
+			<td>${todo.id}
 			<td>${todo.userId }
 			<td>${todo.title}
 			<td>${todo.completed}
-			
+			<td>
+                    <a href="${pageContext.request.contextPath}/todos/delete-todo/${todo.id}" 
+                       onclick="return confirm('Are you sure?')">
+                       Delete
+                    </a>
+                </td>
 		</tr>
 	</c:forEach>
 
